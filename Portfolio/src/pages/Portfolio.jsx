88de56bf7c82baf8843e3bb9b2762960a2503fab
SHMERNIE <fredony.ernesto@gmail.com>
2024-09-14 22:59:59 -0700

    worked on adding repos to my repos page

--- Portfolio/src/pages/Portfolio.jsx
+++ Portfolio/src/pages/Portfolio.jsx
@@ -1,6 +1,41 @@
-export default function Portfolio(){
+import React, { useState, useEffect } from 'react';
+import { fetchRepositories } from '../utils/API';
+import './Repos.css';
+
+export default function Repositories() {
+    const [repos, setRepos] = useState([]);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const getRepositories = async () => {
+            try {
+                const data = await fetchRepositories();
+                setRepos(data);
+            } catch (error) {
+                setError(error);
+            }
+        };
+
+        getRepositories();
+    }, []);
+
+    if (error) return <p>Error: {error.message}</p>;
+
     return (
         <div>
-      </div>
+            <h1 id='my-repos-title'>My GitHub Repositories</h1>
+            <div className="container">
+                <ul className="po_items_ho">
+                    {repos.map(repo => (
+                        <li key={repo.id} >
+                            <a href={repo.html_url} target="_blank" className="po_item">
+                                {repo.name}
+                                <p id="date"> Created: {new Date(repo.created_at).toLocaleDateString()} </p>
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
     );
 }
